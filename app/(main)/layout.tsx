0d0668943d0f3865078696dfcd7d717ae63a7cf9
guiraccone <guilherme.raccone@hotmail.com>
2025-12-30 14:36:28 -0300

    feat: learn page wrapper

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -11,7 +11,7 @@ const MainLayout = ({ children }: Props) => {
             <MobileHeader />
             <Sidebar className="hidden lg:flex"/>
             <main className="lg:pl-64 h-full pt-12.5 lg:pt-0">
-                <div className="bg-red-400 h-full">
+                <div className="max-w-264 mx-auto pt-6 h-full ">
                     {children}
                 </div>
             </main>
--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,7 +1,19 @@
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { StickyWrapper } from "@/components/sticky-wraper";
+import { Header } from "./header";
+import { UserProgress } from "./user-progress";
+import { title } from "process";
+
 const LearnPage = () => {
     return (
-        <div>
-            <p>Learn Page</p>
+        <div className="flex flex-row-reverse gap-12 px-6">
+            <StickyWrapper>
+                <UserProgress activeCourse={{title: "Spanish", imageSrc: "/flag_spain.png"}} hearts={5} points={100} hasActiveSubscription={false} />
+            </StickyWrapper>
+            <FeedWrapper>
+                <Header title="Spanish"/>
+            </FeedWrapper>
+
         </div>
     )
 }
